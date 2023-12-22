891284d8aab3d62e0f73ce0a99d9d902e0ef6fea
SHIRIN <efshitaria123@gmail.com>
2023-12-22 11:43:10 +0600

    task post

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ var cors = require('cors')
 require('dotenv').config()
 var app = express()
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+
+  }
+
+))
 app.use(express.json())
 const port = process.env.PORT || 5000
 
